d7616e4b0c3cb74437fd5872fadb88ec2a622de9
zalex14 <9824018@gmail.com>
2023-01-28 14:55:15 +0300

    Курсовая работа 2 «Java Core»

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Курсовая работа «Java Core»");
+        System.out.println("\nКурсовая работа «Java Core»\n");
     }
 }
